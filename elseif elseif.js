41249cad2cfc3a3538d144afe1d2393c7648f660
reeeshadd <reshadhossen11@gmail.com>
2021-12-23 03:14:05 +0600

    Initial Commit

---
+++ elseif elseif.js
@@ -0,0 +1,13 @@
+let age = 100
+
+if (age < 100) {
+    console.log("Not Eligible")
+}
+
+else if (age === 100) {
+    console.log("Here is your birthday card from king!")
+}
+
+else {
+    console.log("Not eligible! You already got one!")
+}
---
+++ if else.js
@@ -0,0 +1,9 @@
+let age = 21
+
+if (age < 21) {
+    console.log("You're underage! Go Away!")
+}
+
+else {
+    console.log("Welcome Aboard!")
+}
