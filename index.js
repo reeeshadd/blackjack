35a4129282d5095e66c0a103e7174607e1c1381e
reeeshadd <reshadhossen11@gmail.com>
2021-12-23 22:11:52 +0600

    Added function

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ let secondCard = 11
 
 let sum = firstCard + secondCard
 
+let hasBlackJack = false
+let isAlive = true
+
 if (sum < 21) {
     console.log("Do you want to draw a new card? 🙂")
-} 
-
-else if (sum === 21) {
+} else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
-} 
-
-else {
+    hasBlackJack = true
+} else {
     console.log("You're out of the game! 😭")
+    isAlive = false
 }
+
+console.log(hasBlackJack)
+
