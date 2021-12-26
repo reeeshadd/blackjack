00aa5a9b3a7e3ecbd0f8be047701a6047d252b5b
reeeshadd <reshadhossen11@gmail.com>
2021-12-26 09:45:13 +0600

    footer

--- index.js
+++ index.js
@@ -48,9 +48,7 @@ function renderGame() {
     for (i=0;i<cards.length;i++) {
         cardsEl.textContent += cards[i] + ", "
     }
-    if (isAlive === false) {
-        cardsEl.textContent += "."
-    }
+    
     messageEl.textContent = message
 }
 
@@ -75,4 +73,24 @@ function getRandomCard() {
     }else {
         return randomNumber
     }
+}
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
